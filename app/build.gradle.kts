f6323f7e792ddb73ca3c7cadaac865f526ea93c7
Andrey <Foxinthenight@mail.ru>
2024-12-16 15:06:25 +0300

    change sdk target -v 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.andreypmi.musicapp"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -40,12 +40,12 @@ android {
 }
 
 dependencies {
-    implementation ("androidx.media:media:1.4.3")
-    implementation ("androidx.fragment:fragment-ktx:1.5.5")
+    implementation ("androidx.media:media:1.7.0")
+    implementation ("androidx.fragment:fragment-ktx:1.8.5")
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("androidx.lifecycle:lifecycle-service:2.8.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
